9b0a78371b911930a464f019d5ad08ed0446b480
Daf <afahd.ali@gmail.com>
2024-11-14 17:19:12 +0100

    feat: add exception

---
+++ src/DafBowling.Domain/Exceptions/TooManyPinsDownException.cs
@@ -0,0 +1,6 @@
+﻿namespace DafBowling.Domain.Exceptions;
+public class TooManyPinsDownException : Exception
+{
+    public TooManyPinsDownException(string message) : base(message) { }
+    public TooManyPinsDownException() : base() { }
+}
